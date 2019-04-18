c3cd79a8bb9683d2bc2d0b5b32c66ef8987be2b9
sepiropht <sergembotta@mailoo.org>
2019-04-18 21:25:13 +0200

    api call from client work in dev !

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount() {
+      axios.get('/hello/world')
+        .then(({data}) => console.log(' la réponse est ' + data))
+        .catch(err => console.log(err));
+  }
   render() {
     return (
       <div className="App">
